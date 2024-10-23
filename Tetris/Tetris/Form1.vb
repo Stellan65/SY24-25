72ecb3b775423dbc8cad7edda30d17dacfdbacdf
Garryhippo <smstu268@learner.dvusd.org>
2024-10-23 12:28:43 -0700

    i dont remember the changes

--- Tetris/Tetris/Form1.vb
+++ Tetris/Tetris/Form1.vb
@@ -1,12 +1,33 @@
 ﻿Imports System.DirectoryServices
 
 Public Class Form1
+    Dim movements As New Dictionary(Of String, Collection)
+    Dim tracks As New Dictionary(Of String, Integer)
+
+    Dim score As Integer
+    Dim CoinScore As Integer
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles MovementTimer.Tick
-        follow(Enemy2, Avatar, 3, 3)
-        follow(Enemy3, Avatar, 4, 4)
-        PaceX(Enemy, Platform, 5)
+        If score > 150 Then Track(Enemy2Kill, Avatar)
+
+        follow(Enemy3, Avatar, 3, 3)
+        PaceX(EnemyKill, Platform, 4)
+
 
     End Sub
+    Sub Track(e As PictureBox, a As PictureBox)
+        If Not tracks.ContainsKey(e.Name & a.Name) Then
+            tracks.Add(e.Name & a.Name, 1)
+        Else
+            Dim idx As Integer
+            idx = tracks(e.Name & a.Name)
+            If movements.ContainsKey(a.Name) AndAlso idx < movements(a.Name).Count Then
+                e.Location = movements(a.Name).Item(idx)
+                tracks(e.Name & a.Name) = idx + 1
+            End If
+
+        End If
+    End Sub
+
     Sub PaceX(e As PictureBox, p As PictureBox, speed As Integer)
         Dim dir As Integer
         dir = e.Tag
@@ -67,27 +88,89 @@ Public Class Form1
             move(Avatar, 0, 20)
         End If
         Avatar.Refresh()
-        If Avatar.Bounds.IntersectsWith(PictureBox2.Bounds) Then
-            PictureBox2.Visible = False
+        'If Avatar.Bounds.IntersectsWith(PictureBox2.Bounds) And PictureBox2.Visible = True Then
+        '    PictureBox2.Visible = False
+        '    score += 10
+        'End If
+        Dim c As PictureBox
+        If IntersectsWith(Avatar, "coin", c) Then
+            c.Visible = False
+            score += 10
+            CoinsCollected.Text = CoinScore
+            CoinScore += 1
         End If
+
+        If IntersectsWith(Avatar, "Kill") Then
+            DiedScreen.Visible = True
+            LooseLabel.Visible = True
+        End If
+        If CoinScore = 18 Then YOUWIN.Visible = True And DiedScreen.Visible = True
+
     End Sub
     Sub move(p As PictureBox, xdir As Integer, ydir As Integer)
         p.Location += New Point(xdir, ydir)
-    End Sub
 
-    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles Enemy.Click
+        If IntersectsWith(p, "wall") Then
+            p.Location -= New Point(xdir, ydir)
+        End If
 
-    End Sub
-
-    Private Sub Avatar_Click(sender As Object, e As EventArgs) Handles Avatar.Click
-
-    End Sub
-
-    Private Sub Score_Click(sender As Object, e As EventArgs) Handles ScoreLabel.Click
+        If Not movements.ContainsKey(p.Name) Then
+            movements.Add(p.Name, New Collection)
+        End If
+        movements(p.Name).Add(p.Location)
 
     End Sub
+
+    Function endingWith(s As String) As Collection
+        Dim coll As New Collection
+        For Each o In Controls
+            Dim obj As PictureBox
+            obj = TryCast(o, PictureBox)
+            If Not obj Is Nothing Then
+                If UCase(obj.Name).EndsWith(UCase(s)) Then
+                    coll.Add(obj)
+                End If
+            End If
+        Next
+        Return coll
+    End Function
+    Function IntersectsWith(p As PictureBox, tag As String) As Boolean
+        Return IntersectsWith(p, tag, Nothing)
+    End Function
+    Function IntersectsWith(p As PictureBox, tag As String, Optional ByRef other As PictureBox = Nothing) As Boolean
+        For Each o In Controls
+            Dim obj As PictureBox
+            obj = TryCast(o, PictureBox)
+            If Not obj Is Nothing AndAlso obj.Visible Then
+                If p.Bounds.IntersectsWith(obj.Bounds) And (UCase(obj.Tag) = UCase(tag) Or
+                    UCase(obj.Name).EndsWith(UCase(tag))) Then
+                    other = obj
+                    Return True
+                End If
+            End If
+        Next
+        Return False
+    End Function
+
+
+
+
 
     Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles ScoreTimer.Tick
+        If LooseLabel.Visible = False Then
+            score += 1
+            ScoreLabel.Text = score
+        End If
+
+
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub PictureBox33_Click(sender As Object, e As EventArgs)
 
     End Sub
 End Class
