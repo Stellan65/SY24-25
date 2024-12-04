75923eaff11bef4c89c148aa1a4d812aadb3e3ef
Garryhippo <smstu268@learner.dvusd.org>
2024-12-04 12:58:27 -0700

    made a new pokedex program in new language

---
+++ WpfApp2/WpfApp2/MainWindow.xaml.vb
@@ -0,0 +1,18 @@
+﻿Class MainWindow
+
+
+    Private Sub PikaButton_Click(sender As Object, e As RoutedEventArgs) Handles PikaButton.Click
+        Pika.Visibility = Visibility.Visible
+        PikaStats.Visibility = Visibility.Visible
+    End Sub
+
+    Private Sub BulbButton_Click(sender As Object, e As RoutedEventArgs) Handles BulbButton.Click
+        Bulb.Visibility = Visibility.Visible
+        BulbStats.Visibility = Visibility.Visible
+    End Sub
+
+    Private Sub WaterButton_Click(sender As Object, e As RoutedEventArgs) Handles WaterButton.Click
+        Water.Visibility = Visibility.Visible
+        WaterStats.Visibility = Visibility.Visible
+    End Sub
+End Class
